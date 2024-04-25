d89d6d3a765f2b2b28610cc028e71254b98c9682
Aman Sirohi <amansirohi077@gmail.com>
2024-04-26 03:08:43 +0530

    modified about us

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -7,11 +7,10 @@ const page = () => {
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative text-center">
         <h1
             className="max-w-2xl mx-auto text-center font-manrope font-bold text-4xl  text-gray-900 mb-5 md:text-5xl md:leading-normal">
-            Control your Finances with our <span className="text-indigo-600">Smart Tool </span>
+            Virgo Electronics <span className="text-indigo-600">
+              Ghaziabad </span>
         </h1>
-        <p className="max-w-sm mx-auto text-center text-base font-normal leading-7 text-gray-500 mb-9">Invest
-            intelligently
-            and discover a better way to manage your entire wealth easily.</p>
+        <p className="max-w-sm mx-auto text-center text-base font-normal leading-7 text-gray-500 mb-9">Manufacturer of Electrical Appliances</p>
 
 
     </div>
@@ -31,11 +30,7 @@ const page = () => {
                         About
                         Us </h2>
                     <p className="font-normal text-xl leading-8 text-gray-500 max-lg:text-center max-w-2xl mx-auto">
-                        Driven by a passion for seamless user experiences, we&apos;ve meticulously curated pagedone
-                        to empower creators, designers, and developers alike. Our mission is to provide a
-                        comprehensive toolkit,
-                        enabling you to build intuitive, beautiful interfaces that resonate with users on every
-                        interaction.
+                    Virgo has been known for its quality Electrical Appliances. We are one of the leading manufactrures and are notable for our reliable, durable and performance-worthy appliances. The journey of the company began by Mr. Naresh Chand in 2000, when it was established as a small business. Since then, the product advancement and high-quality appliances delivered by us, has always boosted our pace in the market.
                     </p>
                 </div>
             </div>
@@ -52,14 +47,9 @@ const page = () => {
                     <Image src="/virgo.png" alt="virgo logo" width={300} height={300}
                         className="block lg:hidden mb-9 mx-auto "/>
                     <h2 className="font-manrope font-bold text-4xl lg:text-5xl text-black mb-9 max-lg:text-center">We
-                        are Creative Since 2005</h2>
+                        are Creative Since 2001</h2>
                     <p className="font-normal text-xl leading-8 text-gray-500 max-lg:text-center max-w-2xl mx-auto">
-                        Pagedone isn&apos;t just a collection of components and guidelines; it&apos;s a philosophy. We go
-                        beyond aesthetics, prioritizing accessibility, scalability, and usability. Every element,
-                        from the tiniest
-                        detail to the grandest layout, is meticulously crafted to enhance functionality and elevate
-                        user
-                        satisfaction.
+                    With our rich history of quality and reliability, we have, over the years, also earned the trust and reputation from our clients. When it comes to meeting the standards of the industry, staying at par with the quality, our appliances remain at the forefront. Adapting new technologies, exploring quality prospects to develop advanced appliances, remains our continuous motto. Manufacturing electrical home appliances like ceiling fans, table fans, water heater, room heater etc., we understand our responsibility towards our customers. We own our manufacturing plants in (MG Road Industrial Area) (Uttar Pradesh) for meeting the standards and the demands of quality appliances.
                     </p>
                 </div>
             </div>
