e6a6eb512a44fe48fffac393d1b987e563fbe508
Aman Sirohi <amansirohi077@gmail.com>
2024-05-10 18:17:01 +0530

    added gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,43 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { CombinedNavbar } from "@/components/CombinedNavbar";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
+import { GoogleTagManager } from "@next/third-parties/google";
+
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+    subsets: ["latin"],
+    variable: "--font-sans",
+});
 export const metadata: Metadata = {
-  title: "Virgo Electronics",
-  description: "Electronics Appliances manufacturer in Ghaziabad",
-  icons: "/virgo.png"
+    title: "Virgo Electronics",
+    description: "Electronics Appliances manufacturer in Ghaziabad",
+    icons: "/virgo.png",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="">
-      <body  className={cn(
-          "min-h-screen bg-background font-sans antialiased ",
-          fontSans.variable
-        )}>
-      <CombinedNavbar/>
-      {children}
-      <Toaster />
+    return (
+        <html lang="en" className="">
+            <GoogleTagManager gtmId="G-8G3FFRD5HN" />
 
-      <Footer/>
-      </body>
-    </html>
-  );
+            <body
+                className={cn(
+                    "min-h-screen bg-background font-sans antialiased ",
+                    fontSans.variable
+                )}
+            >
+                <CombinedNavbar />
+                {children}
+                <Toaster />
+
+                <Footer />
+            </body>
+        </html>
+    );
 }
